c9b0f76d3c20b009b364e3fbc5fc6d32c3efe69d
Chris Law <claw@spindogs.com>
2019-06-19 12:18:25 +0100

    PhP task questions mostly finished

--- questions.php
+++ questions.php
@@ -15,10 +15,53 @@ through the more basic questions quickly in order to leave time for the more com
 database connection already exists - please ensure any SQL is secure
 
 <form method="post" action="yourscript.php">
+    <span class="error"><?php echo $nameErr; ?></span><br />
+    <span class="error"><?php echo $emailErr; ?></span><br />
     <input type="text" name="name">
     <input type="text" name="email">
     <input type="submit" name="submit" value="Submit">
 </form>
+
+
+<?php
+
+$nameErr = $emailErr = "";
+$name = $email = "";
+
+if ($_SERVER["REQUEST METHOD"] == "POST") {
+    if (empty($_POST["name"])) {
+        $nameErr = "Name is required";
+    }
+    else {
+        $name = text_input($_POST["name"]);
+    }
+
+    if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+    }
+    else {
+        $email = text_input($_POST["email"]);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Invalid email";
+        }
+    }
+
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+
+// create sql table
+$sql = "INSERT INTO Users (name, email) VALUES ($name, $email)"
+
+
+?>
+
 
 # QUESTION 2
 # Based on the data table below, please provide MySQL examples for the following requests:
@@ -38,8 +81,25 @@ database connection already exists - please ensure any SQL is secure
 | 5      | Henry     | Male   | 0            |
 +--------+-----------+--------+--------------+
 
+<!-- get all results -->
+SELECT * FROM Users;
+
+<!-- edit existing record -->
+UPDATE Users;
+SET name = Chris, is_logged_in = 1;
+WHERE id = 2;
+
+<!-- delete record -->
+DELET FROM Users WHERE id = 2;
+
+
+
 # QUESTION 3
 # Looking at the above data table, suggest some appropriate data types and indexes for the columns.
+
+Gender can be one letter e.g. M or F
+
+
 
 # QUESTION 4
 # A new table (see below) links sports to a user. Write a MySQL query to return the
@@ -60,9 +120,22 @@ their name should still appear in the list.
 | 5           | Rugby    | 1            |
 +-------------+----------+--------------+
 
+<!-- query -->
+SELECT Users.name, UserSports.sport
+FROM UserSports
+INNER JOIN Users
+ON Users.id = UserSports.user_id
+WHERE UserSports.is_favourite=1;
+
+
+
 # QUESTION 5
 # Describe in your own words what you would need to do if you wanted to list the name of
 each person alongside all the sports that they play separated by a comma (,)?
+
+
+
+
 
 # QUESTION 6
 # Another table (see below) stores orders for an online shop, please provide MySQL examples
@@ -81,6 +154,17 @@ for the following requests:
 | 3          | 2       | 2015-05-05 00:00:00 | 12.00 |
 | 4          | 3       | 2015-05-20 00:00:00 | 50.00 |
 +------------+---------+---------------------+-------+
+
+<!-- query a -->
+SELECT Users.name, Orders.cost
+FROM Orders
+INNER JOIN Users
+ON Users.id = Orders.user_id
+
+<!-- query b -->
+
+
+
 
 # QUESTION 7
 # The following array contains a number of recipes and their corresponsing ingredients.
@@ -103,13 +187,44 @@ $recipes = array(
         'Lime'
     )
 );
+
+// adding ingredients
+foreach ($recipes as $value) {
+    foreach ($value as $ingredient) {
+        $ingredient["price"] = "0";
+        $ingredient["quantity"] = "0";
+    }
+}
+
+
 ?>
+
 
 # QUESTION 8
 # Write some PHP code to loop through your array and show the following:
 # a) Display the name of each recipe and list the ingredients
 # b) Display the cost of each recipe
 # c) Display the total cost of **all recipes**
+
+<?php
+
+// list name and ingredients
+$total_cost = "";
+foreach ($recipes as $key => $value) {
+    echo "Recipe name: ".$key;
+    $recipe_cost = "";
+    foreach ($value as $ingredient) {
+        $recipe_cost .= $ingredient["price"];
+    }
+    $total_cost .= $recipe_cost;
+    echo "Recipe Cost: ".$recipe_cost;
+}
+echo $total_cost;
+
+?>
+
+
+
 
 # QUESTION 9
 # Take a look at the example below. Please describe any security issues that you identify
@@ -133,6 +248,11 @@ and make a suggestion how the issue could be resolved.
     </table>
 
 <?php } ?>
+
+
+$_GET is insecure
+
+
 
 # QUESTION 10
 # Write some PHP code to list the date of each day, starting with the current date
